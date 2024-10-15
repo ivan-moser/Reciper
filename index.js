ae2c9404720e3d346680f6d53bbeb9d47483a109
ivan-moser <ivaan.moser@gmail.com>
2024-10-15 12:41:07 +0200

    Add: addedd a real random response from the random fetch

--- index.js
+++ index.js
@@ -3,25 +3,28 @@ const appID = '6c321b00';
 const baseUrl = 'https://api.edamam.com/api/recipes/v2';
 
 const randomMeal = async function() {
-    //restituisce un oggetto contenente un array di 20 "hits"
-    const url = `https://api.edamam.com/api/recipes/v2?app_id=${appID}&app_key=${apiKey}&type=public&q=random`;
+    const ingredients = ['chicken', 'pasta', 'salad', 'vegan', 'dessert', 'fish'];
+    const mealsContainer = document.getElementById('results-grid');
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+    mealsContainer.innerHTML = '';
+    document.getElementById('search-input').value = '';
 
-        if (response.ok) {
-            const randomIndex = Math.floor(Math.random() * data.hits.length);
-            const meal = data.hits[randomIndex].recipe;
-            const mealsContainer = document.getElementById('results-grid');
-
-            mealsContainer.innerHTML = '';
-            const input = document.getElementById('search-input').value = '';
-            displayMeal(meal);
+    for (let i = 0; i < 3; i++) {
+        const randomIngredient = ingredients[Math.floor(Math.random() * ingredients.length)];
+        const url = `https://api.edamam.com/api/recipes/v2?app_id=${appID}&app_key=${apiKey}&type=public&q=${randomIngredient}`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+    
+            if (response.ok) {
+                
+                const randomIndex = Math.floor(Math.random() * data.hits.length);
+                const meal = data.hits[randomIndex].recipe;
+                displayMeal(meal);
+            }
+        } catch (error) {
+            console.error('Errore durante la fetch: ',error);
         }
-
-    } catch (error) {
-        console.error('Errore durante la fetch: ',error);
     }
 }
 
